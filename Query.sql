99fbe0147573588f20db8bdb1a4db88a0d931c43
Haseeb-Gondal <97632769+Haseeb-Gondal@users.noreply.github.com>
2024-04-19 15:37:26 +0200

    Created main query first time

---
+++ Query.sql
@@ -0,0 +1,15 @@
+---------------- Test SQL File For Training ------------------
+
+SELECT column1 , column2 
+FROM TEST_TABLE
+WHERE CONDITION_1 = CONDITION_2;
+
+
+
+SELECT column3 , column4 
+FROM TEST_TABLE_2
+WHERE CONDITION_3 = CONDITION_4;
+
+
+
+
